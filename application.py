9258fe9ae6193d769416ead389a039cb6df6c289
Ravi Ranjan <ravi848101mnb@gmail.com>
2023-06-30 21:52:12 +0530

    aws

--- application.py
+++ application.py
@@ -11,11 +11,13 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'])
+@cross_origin()
 def homepage():
     return render_template("index.html")
 
 
 @app.route("/review", methods=['POST', 'GET'])
+@cross_origin()
 def index():
     if request.method == 'POST':
         try:
